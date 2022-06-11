b92e7a9ebc6d33af12d84907f4476899683744ae
Robert Noack <robertnoack4@gmail.com>
2022-06-11 18:44:46 +0200

    Added Parameter Controll and Saving of notes

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,6 +1,58 @@
 import chalk from "chalk"
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 import * as notes from "./notes.js"
 
-const notesText = notes.getNotes()
-console.log(notesText)
-console.log(chalk.green("success!"))
+// Sets arguments for opening app.js
+yargs(hideBin(process.argv))
+    // Creates Add command
+    .command({
+        command: "add",
+        describe: "Add a new note",
+        builder: {
+            // Creates Title Option
+            title: {
+                describe: "Note Title",
+                demandOption: true,
+                type: "string",
+            },
+            body: {
+                describe: "Content of Note",
+                demandOption: true,
+                type: "string"
+            },
+        },
+        // Creates Function to run when add is given as a command
+        handler: function (argv) {
+            notes.addNote(argv.title, argv.body)
+        }
+    })
+    // Creates Remove command
+    .command({
+        command: "remove",
+        describe: "Remove a note",
+        // Creates Function to run when remove is given as a command
+        handler: function () {
+            console.log("Removing the note")
+        }
+    })
+    // Creates List command
+    .command({
+        command: "list",
+        describe: "Shows a list of all Notes",
+        // Creates Function to run when remove is given as a command
+        handler: function () {
+            console.log("Listing all notes")
+        }
+    })
+    // Creates Read command
+    .command({
+        command: "read",
+        describe: "Read a note",
+        // Creates Function to run when read is given as a command
+        handler: function () {
+            console.log("Reading a note")
+        }
+    })
+    .demandCommand(1)
+    .parse()
--- NotesApp/notes.js
+++ NotesApp/notes.js
@@ -1,8 +1,41 @@
 import fs from "fs"
-fs.writeFileSync("notes.txt", "Dieser Text wurde mit dem File erstellt.")
-fs.appendFileSync("notes.txt", "\nDieser Text wurde hinzugefuegt.");
 
 export function getNotes() {
     const text = fs.readFileSync("notes.txt", "utf-8");
     return text;
+}
+
+export function addNote(title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body,
+        })
+        saveNotes(notes)
+        console.log("Note added")
+    }
+    else {
+        console.log("Note Title taken")
+    }
+
+}
+
+function saveNotes(notes) {
+    const dataJson = JSON.stringify(notes)
+    fs.writeFileSync("notes.json", dataJson)
+}
+
+function loadNotes() {
+    try {
+        const dataBuffer = fs.readFileSync("notes.json")
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e) {
+        return[]
+    }
 }
